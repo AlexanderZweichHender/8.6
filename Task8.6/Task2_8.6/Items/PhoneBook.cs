2ec89e68413906e216ee02e6396b4e9708200815
AlexanderZweichHender <93994885+AlexanderZweichHender@users.noreply.github.com>
2023-08-18 13:54:49 -0700

    task 1 & task2

--- Task8.6/Task2_8.6/Items/PhoneBook.cs
+++ Task8.6/Task2_8.6/Items/PhoneBook.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json;
+﻿using Newtonsoft.Json;
 
 namespace Task2_8._6.Items
 {
@@ -20,29 +20,60 @@ namespace Task2_8._6.Items
             }
             else
             {
-                _numbers = DeserializeNumbers();
+                try
+                {
+                    _numbers = DeserializeNumbers();
+                }
+                catch
+                {
+                    _numbers = new Dictionary<string, string>();
+                }
             }
+        }
+
+        public void AddNumber()
+        {
+            Console.Write("Enter number: ");
+            var number = Console.ReadLine();
+
+            Console.Write("Enter name: ");
+            var name = Console.ReadLine();
+
+            _numbers.Add(number, name);
+            SerializeNumbers();
         }
 
         public void SerializeNumbers()
         {
-            using (var fileStream = new FileStream(_path, FileMode.Append, FileAccess.Write))
-            {
-                JsonSerializer.Serialize(fileStream, _numbers);
-            }
+            var data = JsonConvert.SerializeObject(_numbers);
+            File.WriteAllText(_path, data);
         }
 
         public Dictionary<string, string> DeserializeNumbers()
         {
-            using(var fileStream = new FileStream(_path, FileMode.Open, FileAccess.Read))
-            {
-                return JsonSerializer.Deserialize<Dictionary<string, string>>(fileStream);
-            }
+            var data = File.ReadAllText(_path);
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
         }
 
-        public void ShowNumbers(Dictionary<string, string> numbers)
+        public void ShowContactByNumber()
         {
-            //numbers.For
+            Console.Write("Enter number to find: ");
+            var number = Console.ReadLine();
+
+            Console.WriteLine(_numbers.FirstOrDefault(contact =>
+                contact.Key == number));
         }
+
+        public void RemoveContact()
+        {
+            Console.Write("Enter contact to remove: ");
+            var key = Console.ReadLine();
+
+            _numbers = _numbers.Where(number => number.Key != key && number.Value != key)
+                .ToDictionary(number => number.Key, x => x.Value);
+            SerializeNumbers();
+        }
+        public void ShowNumbers()
+            => _numbers.ToList().ForEach(number => Console.WriteLine(number));
     }
 }
--- Task8.6/Task2_8.6/Program.cs
+++ Task8.6/Task2_8.6/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Solution
+﻿using Task2_8._6.Items;
+
+namespace Solution
 {
     internal class Program
     {
+        private static readonly string _path = "PhoneNumbers.txt";
+
         private static void Main()
         {
-            
+            var phoneBook = new PhoneBook(_path);
+
+            while (true)
+            {
+                Console.Write("Enter operation number:\n0 - quit\n1 - show all numbers\n" +
+                    "2 - add new number\n3 - find contact\n4 - remove contact\n");
+                
+                switch(Console.ReadLine())
+                {
+                    case "1":
+                        phoneBook.ShowNumbers();
+                        break;
+                    case "2":
+                        phoneBook.AddNumber();
+                        break;
+                    case "3":
+                        phoneBook.ShowContactByNumber();
+                        break;
+                    case "4":
+                        phoneBook.RemoveContact();
+                        break;                       
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Incorrect input");
+                        break;
+                }
+            }           
         }
     }
 }
